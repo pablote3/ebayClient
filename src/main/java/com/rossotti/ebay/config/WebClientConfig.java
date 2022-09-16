60b21bf09dda8d38b855eb325f645d3c1431fae9
Paul Rossotti <rossotti.paul@gmail.com>
2022-09-16 10:07:37 -0700

    add groupId for .m2 repo path

--- src/main/java/com/rossotti/ebay/config/WebClientConfig.java
+++ src/main/java/com/rossotti/ebay/config/WebClientConfig.java
@@ -8,7 +8,6 @@ import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class WebClientConfig {
-
     @Bean
     public WebClient createWebClient() {
         return WebClient.builder()
